e7e6b7664f0c7c1681c94a7df1d21f0e3e2eee92
Mike Weatherford <mikesweatherford@gmail.com>
2022-10-26 18:55:23 -0400

    first commit

---
+++ KnowledgeCheckTwo/ArtForm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnowledgeCheckTwo
+{
+    internal abstract class ArtForm
+    {
+        public string ArtistName { get; set; }
+    }
+}
---
+++ KnowledgeCheckTwo/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace KnowledgeCheckTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many records do you want to add? ");
+            var numberOfRecords = int.Parse(Console.ReadLine());
+
+
+
+            var recordList = new List<Music>();
+            for (int i = 0; i < numberOfRecords; i++)
+            {
+                // In this loop, populate the object's properties using Console.ReadLine()
+                var entry = new Music();
+
+                Console.WriteLine("Enter your Band Name: ");
+                entry.ArtistName = Console.ReadLine();
+
+                Console.WriteLine("Enter the Genre: ");
+                entry.Genre = Console.ReadLine();
+
+                recordList.Add(new Music() { ArtistName = entry.ArtistName, Genre = entry.Genre });
+            }
+
+            // Print out the list of records using Console.WriteLine()
+            foreach (var entry in recordList)
+                Console.WriteLine("ARIST: " + entry.ArtistName + "\nGENRE: " + entry.Genre + "\n");
+
+        }
+    }
+}
